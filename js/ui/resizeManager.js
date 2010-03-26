7dff82cc1b9611b08ffc8d406c0c2b35feafc3ff
Marcus Westin <marcus.westin@gmail.com>
2010-03-25 18:40:33 -0700

    Add a helpful message to the background. Thanks Douglas.

--- js/ui/resizeManager.js
+++ js/ui/resizeManager.js
@@ -5,6 +5,8 @@ exports = Singleton(function(){
 	this.init = function() {
 		this._resizeCallbacks = []
 		window.onresize = bind(this, '_onResize')
+		
+		this._dontPanic()
 	}
 	
 	this._onResize = function() {
@@ -34,6 +36,23 @@ exports = Singleton(function(){
 	
 	this.fireResize = function() {
 		this._onResize()
+	}
+	
+	this._dontPanic = function() {
+		var doc = document,
+			el = doc.body.appendChild(doc.createElement('div'))
+		el.style.fontSize = '82px'
+		el.style.color = '#e9e9e9'
+		el.style.fontFamily = 'Verdana'
+		el.style.fontStyle = 'Italic'
+		el.style.position = 'absolute'
+		el.innerHTML = "Don't Panic"
+		this.addDependant(function(winSize) {
+			var width = el.offsetWidth
+			var height = el.offsetHeight
+			el.style.right = ((winSize.width - 500) / 2) - (width / 2) + 'px' // in the middle on the right off of the panels
+			el.style.top = (winSize.height / 2) - (height / 2) + 'px'
+		})
 	}
 	
 })

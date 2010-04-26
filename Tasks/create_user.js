3bd7e01a05d19515f5511a683d3c0e90ad1492b3
Marcus Westin <marcus.westin@gmail.com>
2010-04-25 20:38:52 -0700

    Add a script to create users from the command line with a fake prefilled password. For developing for now

---
+++ Tasks/create_user.js
@@ -0,0 +1,20 @@
+#!/usr/bin/env node
+
+require('../lib/fin/lib/js.io/packages/jsio')
+
+jsio.path.shared = '../lib/fin/js/'
+jsio.path.server = '../lib/fin/js/'
+jsio.path.client = '../lib/fin/js/'
+jsio.path.fan = '../'
+
+jsio('import client.fin') // makes fin globally accesible
+jsio('import fan.sha1')
+
+
+fin.registerEventHandler('FAN_AUTHENTICATION_RESPONSE', function() {
+	process.exit()
+})
+
+fin.connect(function() {
+	fin.send('FAN_REQUEST_CREATE_USER', { email: process.argv[2], password_hash: fan.sha1('123123') })
+})

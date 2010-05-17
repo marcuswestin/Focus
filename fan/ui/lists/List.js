f0171266356405f199c8c5ff43f3016493e5ee68
Marcus Westin <marcus.westin@gmail.com>
2010-05-16 17:33:39 -0700

    Add a discussion area to each task

--- fan/ui/lists/List.js
+++ fan/ui/lists/List.js
@@ -36,20 +36,30 @@ exports = Class(fan.ui.Component, function(supr){
 		this._render()
 	}
 	
-	this._render = function() {
+	this.append = function(items) {
+		Array.prototype.push.apply(this._items, items)
+		this._render()
+	}
+	this.prepend = function(items) {
+		Array.prototype.unshift.apply(this._items, items)
+		this._render()
+	}
+	
+	this.createDelayedMethod('_render', function() {
 		if (!this._element || !this._items) { return }
 		for (var i=0, item; item = this._items[i]; i++) {
 			this._insertElement(this._getCellFor(item), i)
 		}
-	}
+	})
 	
 	this._getCellFor = function(label) {
 		var cell = this._cells[label]
 		
 		if (cell) { return cell }
-		cell = this._create({ parent: this._element, text: label.replace(/_/g, ' '),
-				className: 'cell ' + label.toString().replace(/ /g, '-').toLowerCase() })
+		var text = label.replace(/_/g, ' '),
+			className = 'cell ' + label.toString().replace(/ /g, '-').toLowerCase()
 		
+		cell = this._create({ text: text, className: className })
 		cell.delegateId = label
 		
 		return (this._cells[label] = cell)

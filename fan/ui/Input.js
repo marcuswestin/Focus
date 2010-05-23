ad90d8638b1b0284f2a633dd3f204cc127f24d68
Marcus Westin <marcus.westin@gmail.com>
2010-05-23 14:21:28 -0700

    Clean up Input code, blur on escape, and fix bug where keydown/keyup keycodes were mixed up

--- fan/ui/Input.js
+++ fan/ui/Input.js
@@ -25,20 +25,34 @@ exports = Class(fan.ui.Component, function(supr) {
 		}
 	}
 	
-	this._createContent = function() {
-		this._element.value = this._defaultText;
-		this._on('focus', bind(this, '_onFocus'));
-		this._on('blur', bind(this, '_onBlur'));
-		this._on('keydown', bind(this, '_onKeyDown'))
-		this.setText()
-	}
-	
 	this.getValue = function() { return this._element.value }
 	this.focus = function() { this._element.focus() }
 	this.disable = function() { this._element.disabled = true }
 	this.clear = function() { setTimeout(bind(this._element, function(){ this.value = '' })) }
 	this.blur = function() { this._element.blur() }
 	
+	this._createContent = function() {
+		this._element.value = this._defaultText;
+		this._on('keydown', bind(this, '_onKeyDown'))
+		this._on('focus', bind(this, '_onFocus'));
+		this._on('blur', bind(this, '_onBlur'));
+		this.setText()
+		
+		var keys = this.keys,
+			keyMap = this._keyMap = {}
+		
+		keyMap[keys['enter']] = bind(this, '_publish', 'Submit')
+		keyMap[keys['escape']] = bind(this, 'blur')
+	}
+	
+	this._onKeyDown = function(e) {
+		var keyHandler = this._keyMap[e.keyCode]
+		if (keyHandler) {
+			e.cancel()
+			keyHandler()
+		}
+	}
+
 	this._onFocus = function() {
 		gKeyboardFocus.grabFocus(this)
 		if (this._element.value != this._defaultText) { return; }
@@ -53,11 +67,5 @@ exports = Class(fan.ui.Component, function(supr) {
 		if (this._isPassword) { this._element.type = 'text'; }
 		this.addClassName('defaultValue');
 		this.setText(null, true)
-	}
-	
-	this._onKeyDown = function(e) {
-		if (e.keyCode == this.keys['enter']) {
-			this._publish('Submit')
-		}
 	}
 })

5278b211c7ba990878cfc86966ffdb1fe850d44c
Marcus Westin <marcus.westin@gmail.com>
2010-05-27 08:12:13 -0700

    Create custom buttons, instead of using the native button UI

--- fan/ui/Button.js
+++ fan/ui/Button.js
@@ -4,7 +4,6 @@ jsio('import fan.ui.Component')
 // Do something like http://sexybuttons.googlecode.com/svn/trunk/index.html#
 exports = Class(fan.ui.Component, function(supr) {
 	
-	this._domTag = 'button'
 	this._className = 'Button'
 	
 	this.init = function(text) {
@@ -19,6 +18,13 @@ exports = Class(fan.ui.Component, function(supr) {
 	
 	this._createContent = function() {
 		this.setText()
+		this._on('mousedown', bind(this, '_onMouseDown'))
+		this._on('mouseup', bind(this, '_onMouseUp'))
+		this._on('mouseout', bind(this, '_onMouseOut'))
 		this._on('click', bind(this, '_publish', 'Click'))
 	}
+	
+	this._onMouseDown = function() { this.addClassName('down') }
+	this._onMouseUp = function() { this.removeClassName('down') }
+	this._onMouseOut = function() { this.removeClassName('down') }
 })

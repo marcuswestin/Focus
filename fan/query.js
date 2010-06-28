620c7af41ffd2fd3cc005f711527477f1c2e65a1
Marcus Westin <marcus.westin@gmail.com>
2010-06-28 09:06:35 -0700

    Add query file, which is used in a bunch of places. Oops\!

---
+++ fan/query.js
@@ -0,0 +1,17 @@
+jsio('from shared.javascript import bind, Singleton')
+jsio('import shared.Publisher')
+
+var query = exports = new shared.Publisher(),
+	_location = window.location
+
+query.getHash = function() { return _location.hash.substr(1) }
+query.setHash = function(hash) { _location.hash = '#' + hash }
+
+var currentHash = query.getHash()
+setInterval(function() {
+	var hash = query.getHash()
+	if (hash == currentHash) { return }
+	var oldHash = currentHash
+	currentHash = hash
+	query._publish('HashChanged', currentHash, oldHash)
+}, 100)

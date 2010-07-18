9f69aa2bb7a9e7519b7038abcb396c3e44fb7b63
Marcus Westin <marcus.westin@gmail.com>
2010-07-18 15:24:33 -0700

    Fix the user creation script

--- node_scripts/create_user.js
+++ node_scripts/create_user.js
@@ -1,20 +1,17 @@
-#!/usr/bin/env node
+var sys = require('sys')
+require('../lib/fin/fin')
 
-require('../lib/fin/lib/js.io/packages/jsio')
+jsio.path.fan = '..'
 
-jsio.path.shared = './lib/fin/js/'
-jsio.path.server = './lib/fin/js/'
-jsio.path.client = './lib/fin/js/'
-jsio.path.fan = '.'
-
-jsio('import client.fin') // makes fin globally accesible
 jsio('import fan.sha1')
 
+var email = process.argv[2]
 
 fin.registerEventHandler('FAN_AUTHENTICATION_RESPONSE', function() {
+	sys.puts("Sucessfully create user with email " + email)
 	process.exit()
 })
 
 fin.connect(function() {
-	fin.send('FAN_REQUEST_CREATE_USER', { email: process.argv[2], password_hash: fan.sha1('123123') })
+	fin.send('FAN_REQUEST_CREATE_USER', { email: email, password_hash: fan.sha1('123123') })
 })

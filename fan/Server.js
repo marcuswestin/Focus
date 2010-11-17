a9652fe5c03db246d7a4859c36105e21938e988c
Marcus Westin <marcus.westin@gmail.com>
2010-11-16 20:50:10 -0800

    Use new fin

--- fan/Server.js
+++ fan/Server.js
@@ -12,18 +12,15 @@ exports = Class(server.Server, function(supr) {
 			passwordHash = args.password_hash,
 			idToKey = fan.keys.userEmailToId(facebookID)
 		
-		this._redisClient.setnx(idToKey, '__fan_tmp_holder', bind(this, function(err, wasCreated) {
+		this._store.setIfNull(idToKey, '__fan_tmp_holder', bind(this, function(err, wasCreated) {
 			if (err) { throw logger.error('Could not create user email to id ', facebookID, idToKey, err) }
-			if (!wasCreated) { 
-				callback(null, 'user with id ' + facebookID + ' already exists') 
-				return
-			}
+			if (!wasCreated) { return callback(null, 'user with id ' + facebookID + ' already exists') }
 			var userProps = { 'password_hash': passwordHash, 'facebookID': facebookID, 
 				type: 'user', iconUrl: '//graph.facebook.com/' + facebookID + '/picture', name: name || facebookID }
 			
 			logger.log("Create user with items", userProps)
 			this.createItem(userProps, origConnection, bind(this, function(newItemId) {
-				this._redisClient.set(idToKey, newItemId, bind(this, function(err) {
+				this._store.set(idToKey, newItemId, bind(this, function(err) {
 					if (err) { throw logger.error('Could not update user email to id', idToKey, newItemId, err) }
 					callback(newItemId)
 				}))
@@ -35,7 +32,8 @@ exports = Class(server.Server, function(supr) {
 		if (!inputEmail || !inputPasswordHash) { return callback(null, 'Please give us both email and password') }
 		var userIdKey = fan.keys.userEmailToId(inputEmail)
 		
-		this._redisClient.get(userIdKey, bind(this, function(err, userIdBytes) {
+		
+		this._store.getBytes(userIdKey, bind(this, function(err, userIdBytes) {
 			if (err) { throw logger.error('Could not retrieve id for key', userIdKey, err) }
 			if (!userIdBytes) { return callback(null, 'We couldn\'t find that email in our db') }
 			var userId = userIdBytes.toString(),
@@ -43,7 +41,7 @@ exports = Class(server.Server, function(supr) {
 			
 			this._retrieveBytes(key, bind(this, function(passwordHash, key) {
 				var response = (passwordHash == JSON.stringify(inputPasswordHash)) ? userId : null
-				callback(response, 'That\'s not the right password')
+				callback(response, 'That\'s not the right password ' + passwordHash + ' ' + inputPasswordHash)
 			}))
 		}))
 	}
--- node_scripts/run_server.js
+++ node_scripts/run_server.js
@@ -1,7 +1,5 @@
 require('../lib/fin/lib/js.io/packages/jsio')
 
-var redis = require('../lib/fin/lib/redis-node-client/lib/redis-client')
-
 jsio.addPath('./lib/fin/js', 'shared')
 jsio.addPath('./lib/fin/js', 'server')
 jsio.addPath('.', 'fan')
@@ -9,7 +7,8 @@ jsio.addPath('.', 'fan')
 jsio('import fan.Server')
 jsio('import fan.Connection')
 
-var fanServer = new fan.Server(fan.Connection, redis)
+var redisEngine = require('../lib/fin/engines/redis')
+var fanServer = new fan.Server(fan.Connection, redisEngine)
 
 fanServer.listen('csp', { port: 5555 }) // for browser clients
 fanServer.listen('tcp', { port: 5556, timeout: 0 }) // for robots

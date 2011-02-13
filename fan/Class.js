4774b26e74011fb180f5a07ac334e83c4c4ef90e
Marcus Westin <marcus.westin@gmail.com>
2011-02-12 20:32:19 -0500

    Add Class

---
+++ fan/Class.js
@@ -0,0 +1,37 @@
+var Class = module.exports = function(parent, proto) {
+	if(!proto) { proto = parent }
+	proto.prototype = parent.prototype
+	
+	var cls = function() { if(this.init) { this.init.apply(this, arguments) }}
+	cls.prototype = new proto(function(context, method, args) {
+		var target = parent
+		while(target = target.prototype) {
+			if(target[method]) {
+				return target[method].apply(context, args || [])
+			}
+		}
+		throw new Error('supr: parent method ' + method + ' does not exist')
+	})
+	
+	// Sometimes you want a method that renders UI to only execute once if it's called 
+	// multiple times within a short time period. Delayed methods do just that
+	cls.prototype.createDelayedMethod = function(methodName, fn) {
+		// "this" is the class
+		this[methodName] = function() {
+			// now "this" is the instance. Each instance gets its own function
+			var executionTimeout
+			this[methodName] = bind(this, function() {
+				clearTimeout(executionTimeout)
+				executionTimeout = setTimeout(bind(fn, 'apply', this, arguments), 10)
+			})
+			this[methodName].apply(this, arguments)
+		}
+	}
+	
+	cls.prototype.constructor = cls
+	return cls
+}
+
+Class.Singleton = function(parent, proto) {
+	return new (Class(parent, proto))()
+}

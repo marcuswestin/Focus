4c4b59de4a3a10a6906eb0bc30b1dc998452c8f8
Marcus Westin <marcus.westin@gmail.com>
2011-02-27 15:15:18 -0500

    In the main app script, create the panels and lay them out whenever the window resizes

--- js/app_new.js
+++ js/app_new.js
@@ -1,7 +1,8 @@
 require('./globals')
 
 var util = require('../lib/fin/api/fin/util'),
-	panels = require('./ui/panels')
+	panels = require('./ui/panels'),
+	resizeManager = require('./ui/resizeManager')
 
 fin.handle('authenticate', function() {
 	fin.request('login', { uid:'marcus' })
@@ -11,15 +12,40 @@ fin.handle('authentication', function(data) {
 	if (!data.uid) { throw 'could not log in' }
 	global.user = new models.User(data.uid)
 	
+	reflectCurrentTaskInHash()
+	createPanels()
+	resizeManager.onResize(layoutPanels)
+	models.local.currentTaskID.observe(layoutPanels)
+})
+
+fin.connect()
+
+/* Misc
+ ******/
+function createPanels() {
 	global.panels = {
 		left: new panels.LeftPanel(),
 		center: new panels.CenterPanel(),
 		right: new panels.RightPanel()
 	}
-	
-	util.each(global.panels, function(panel) {
-		panel.appendTo(document.body);
-	})
-})
+	util.each(global.panels, function(panel) { panel.appendTo(document.body) })
+}
 
-fin.connect()
+function layoutPanels() {
+	var winSize = resizeManager.getWindowSize(),
+		margin = 10,
+		left = margin
+	util.each(global.panels, function(panel) {
+		panel.setStyle({ left:left })
+		left += panel.resize(winSize.w - left, winSize.h - 20) + margin
+	})
+}
+
+function reflectCurrentTaskInHash() {
+	models.local.currentTaskID.observe(function(taskID) {
+		if (!taskID) { return }
+		window.location.hash = '#' + (taskID || '')
+	})
+	var taskID = window.location.hash.substr(1)
+	if (taskID) { models.local.currentTaskID.set(parseInt(taskID)) }
+}

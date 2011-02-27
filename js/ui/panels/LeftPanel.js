d62e9f31fe83120f5fade44c9bd93cc7f79c2a68
Marcus Westin <marcus.westin@gmail.com>
2011-02-27 15:08:56 -0500

    Remove unused and commented code

--- js/ui/panels/LeftPanel.js
+++ js/ui/panels/LeftPanel.js
@@ -29,48 +29,4 @@ module.exports = Class(Panel, function(supr) {
 		if (!views[name]) { views[name] = new payload.Constructor() }
 		this._setView(views[name])
 	}
-	
-	this._createTask = function() {
-		var task = new models.Task({ title:"Task", owner:global.user }).create()
-		global.user.tasks.add(task)
-	}
 })
-
-// var Panel = require('./Panel'),
-// 	TasksListView = require('../views/TasksListView'),
-// 	CalendarView = require('../views/CalendarView'),
-// 	NewsView = require('../views/NewsView'),
-// 	RadioButtons = require('../../ui/RadioButtons')
-// 
-// module.exports = Class(Panel, function(supr) {
-// 
-// 	this._className += ' ListPanel'
-// 	
-// 	this._viewCtors = {
-// 		'tasks': TasksListView,
-// 		// 'projects': fan.tasks.views.ProjectsListView,
-// 		'calendar': CalendarView,
-// 		'news': NewsView,
-// 		// 'coworkers': fan.tasks.views.CoworkersView,
-// 		// 'accomplishments': fan.tasks.views.AccomplishmentsView
-// 	}
-// 	
-// 	this._createContent = function() {
-// 		supr(this, '_createContent')
-// 		this._element.style.left = '30px';
-// 		
-// 		this._views = {}
-// 		
-// 	}
-// 	
-// 	this._getButton = function(id) { return { payload: id, className: id } }
-// 	
-// 	this._onWindowResize = function(winSize) {
-// 		supr(this, '_onWindowResize', arguments)
-// 		
-// 		this._lastMaxWidth = winSize.width
-// 		if (!this._currentView) { return }
-// 		this._resize()
-// 	}
-// 	
-// })

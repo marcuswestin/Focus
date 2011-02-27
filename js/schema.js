dfef641c0bb220a488929eb7029dfb1c0c84b0b7
Marcus Westin <marcus.westin@gmail.com>
2011-02-27 15:59:25 -0500

    Ah... Since we don't have optional properties yet, the instantiation of a task waits for its parent task, which will never arrive...

--- js/schema.js
+++ js/schema.js
@@ -8,8 +8,8 @@ module.exports = {
 		"labels":      { id:2, type:"Set", of:"TaskLabel" },
 		"owner":       { id:3, type:"User" },
 		"subscribers": { id:4, type:"Set", of:"User" },
-		"parent":      { id:5, type:"Task" },
-		"subTasks":    { id:6, type:"Set", of:"Task" },
+		// "parent":      { id:5, type:"Task" },
+		// "subTasks":    { id:6, type:"Set", of:"Task" },
 		"due":         { id:7, type:"Number" },
 		"description": { id:8, type:"Text" }
 	},
